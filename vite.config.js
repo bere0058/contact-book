066f4adfdd733e2229ccf869fd32f7ea5078e2cc
Guillermo Bereche <bere0058@algonquinlive.com>
2022-08-27 17:37:33 -0400

    s

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const BASE_URL = '/contact-book/'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: import.meta.env.BASE_URL
+  base: '/contact-book/'
 })
